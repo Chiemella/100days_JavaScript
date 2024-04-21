98081dab4f2fc5e59ba84fde8ead16d0bbcd7a9e
chiemela <williamschiemelaod@gmail.com>
2024-04-21 21:11:28 +0100

    done

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,2 +1,19 @@
 let Hey  = 'Hello';
-if (Hey === 'Hello') alert('Hey sir, good afternoon');
+// if (Hey === 'Hello') alert('Hey sir, good afternoon');
+
+// Values are specifically the smallest unit of informations we have in JavaScript like '1 and Jona' one of the useful cases is to store them in varaibles which means we could reuse them over and over again.
+
+console.log(1+1);
+console.log('Jona');
+console.log(23)
+
+// Declaring a variable
+let firstName = 'Jona'
+// firstName is the name of the variable while 'Jona' is the value assigned to the variable
+
+//Varibbales names can not stand with a number
+
+console.log(firstName)
+
+// Assignment
+
