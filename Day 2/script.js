8dfb22b63d08ae6a3bf462dedeabbb795959b992
chiemela <williamschiemelaod@gmail.com>
2024-04-22 09:48:01 +0100

    assignment completed

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,19 +1,33 @@
-let Hey  = 'Hello';
-// if (Hey === 'Hello') alert('Hey sir, good afternoon');
+// Define a greeting
+let Hey = 'Hello';
 
-// Values are specifically the smallest unit of informations we have in JavaScript like '1 and Jona' one of the useful cases is to store them in varaibles which means we could reuse them over and over again.
+// Display greeting if it matches 'Hello'
+// if (Hey === 'Hello') {
+//     alert('Hey sir, good afternoon');
+// }
 
-console.log(1+1);
+// Values are specifically the smallest unit of information we have in JavaScript, like '1 and Jona'. 
+// One of the useful cases is to store them in variables, which means we could reuse them over and over again.
+
+// Display some values
+console.log(1 + 1);
 console.log('Jona');
-console.log(23)
+console.log(23);
 
 // Declaring a variable
-let firstName = 'Jona'
-// firstName is the name of the variable while 'Jona' is the value assigned to the variable
+let firstName = 'Jona';
+// firstName is the name of the variable, while 'Jona' is the value assigned to the variable.
 
-//Varibbales names can not stand with a number
+// Variable names cannot start with a number
 
-console.log(firstName)
+console.log(firstName);
 
 // Assignment
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = '200 million';
 
+// Log country, continent, and population to the console
+console.log(country);
+console.log(continent);
+console.log(population);
