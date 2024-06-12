f867baa3db92bfb0aaba67b4b0d2bd6b9767131f
chiemella <williamschiemelaod@gmail.com>
2024-06-12 21:27:19 +0100

    updated

--- Day 4/script.js
+++ Day 4/script.js
@@ -1,6 +1,15 @@
+
+
+
+
+
+
+
+
+
+
 // let myAge = 50
 // let humanDogRatio = 150
-
 // let myDogAge = myAge * humanDogRatio
 // console.log(myDogAge);
 
@@ -8,16 +17,33 @@
 
 // let firstbatch = 5
 // let secondBatch = 5
-
 // let count = firstbatch + secondBatch
-
 // console.log(count);
 
 
-let him = 20
-let her = 50 
+// let him = 20
+// let her = 50 
+// let thereAge = him + her 
+// console.log(thereAge)
 
-let thereAge = him + her 
 
-console.log(thereAge)
+// let count = 20
+// count = count + 50
+// console.log(count)
+
+
+
+
+// let bonusPoint = 50
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint + 50
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint - 75
+// console.log(bonusPoint)
+
+// bonusPoint = bonusPoint + 45
+// console.log(bonusPoint)
+
 
